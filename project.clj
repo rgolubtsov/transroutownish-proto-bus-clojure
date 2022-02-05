088eb3b27bc1b52ca956f2d28ae380965acd29f8
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2022-02-05 23:50:00 +1100

    Update Clojure to version 1.10.3.

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
         :url  "https://raw.githubusercontent.com/rgolubtsov/transroutownish-proto-bus-clojure/main/LICENSE"
     }
     :dependencies [
-        [org.clojure/clojure       "1.10.1"]
+        [org.clojure/clojure       "1.10.3"]
         [org.clojure/tools.logging "1.2.4" ]
         [org.slf4j/slf4j-reload4j  "1.7.35"]
     ]
